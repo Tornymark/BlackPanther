7004d73abb3b6425d4fbff6288539729b43987aa
Tornymark <89776581+Tornymark@users.noreply.github.com>
2021-09-01 16:35:44 +0530

    Update qr.js

--- qr.js
+++ qr.js
@@ -7,25 +7,25 @@ WhatsAsena - Yusuf Usta
 */
 
 const chalk = require('chalk');
-const WhatsAsenaStack = require('whatsasena-npm');
-const {WAConnection, MessageOptions, MessageType, Mimetype} = require('@adiwajshing/baileys');
+const {WAConnection} = require('@adiwajshing/baileys');
 const {StringSession} = require('./whatsasena/');
 const fs = require('fs');
 
 async function whatsAsena () {
     const conn = new WAConnection();
     const Session = new StringSession();  
-    conn.version = [2, 2126, 14]
     conn.logger.level = 'warn';
-    conn.regenerateQRIntervalMs = 50000;
+    conn.regenerateQRIntervalMs = 30000;
     
     conn.on('connecting', async () => {
         console.log(`${chalk.green.bold('Whats')}${chalk.blue.bold('Asena')}
 ${chalk.white.italic('AsenaString Kodu Alıcı')}
 
-${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
+${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please wait.')}`);
     });
-    conn.on('open', async () => {
+    
+
+    conn.on('open', () => {
         var st = Session.createStringSession(conn.base64EncodedAuthInfo());
         console.log(
             chalk.green.bold('Asena String Kodunuz: '), Session.createStringSession(conn.base64EncodedAuthInfo())
@@ -34,11 +34,14 @@ ${chalk.blue.italic('ℹ️  Connecting to Whatsapp... Please Wait.')}`);
         if (!fs.existsSync('config.env')) {
             fs.writeFileSync('config.env', `ASENA_SESSION="${st}"`);
         }
-        console.log(st)
-        var msg = await WhatsAsenaStack.qr(conn.user.jid, conn.user.name)
-        console.log(msg)
+
+        console.log(
+            chalk.blue.bold('Locale kuruyorsanız node bot.js ile botu başlatabilirsiniz.')
+        );
         process.exit(0);
     });
+
     await conn.connect();
 }
+
 whatsAsena()
