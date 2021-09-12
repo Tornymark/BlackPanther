5d03494e74ee44fbc2975772ce2124db9169cf8a
Tornymark <89776581+Tornymark@users.noreply.github.com>
2021-09-12 17:05:51 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -240,13 +240,13 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
                             await command.function(whats, match);
                         } catch (error) {
                             if (config.LANG == 'EN' || config.LANG == 'ML') {
-                                await conn.sendMessage(conn.user.jid, fs.readFileSync("./src/image/WhatsAlexa.png")' +
+                                await conn.sendMessage(conn.user.jid, fs.readFileSync("./src/image/WhatsAlexa.png") +
                                 '\n*WhatsAlexa an error has occurred!*'+
                                 '\n*Report this error to the developer! [ TOXIC-DEVIL ]._*' +
                                 '\n*Error:* ```' + error + '```\n\n'
                                 , MessageType.image);
                          } else {
-                                await conn.sendMessage(conn.user.jid, fs.readFileSync("./src/image/WhatsAlexa.png")' +
+                                await conn.sendMessage(conn.user.jid, fs.readFileSync("./src/image/WhatsAlexa.png") +
                                 '\n\n*🧞‍♂️ ' + error + '*\n'
                                 , MessageType.image);
                             }
